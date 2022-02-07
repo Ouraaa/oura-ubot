5b07ac9d44551487300358086816f343e45d3f48
Ouraaa <99151754+Ouraaa@users.noreply.github.com>
2022-02-08 00:21:37 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -212,7 +212,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Bdrl](https://t.me/TvoChanel)",
+        \na Join Channel [oura](https://t.me/TvoChanel)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
